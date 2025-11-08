a9c8c9bda7f16e97f440f9fe6222325a602c3f5d
milanmitreski <milanmitreski@gmail.com>
2025-11-08 18:08:46 +0100

    First week materials uploaded.

---
+++ materials/v01/main.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+
+struct TrieNode {
+    public:
+        TrieNode(): wordEnd(false) {}
+        ~TrieNode() { this->free(); }
+
+        bool find(const std::string word) {
+            return find(word, 0);
+        }
+
+        void insert(const std::string& word) {
+            this->insert(word, 0);
+        }
+
+        void printAllWords() {
+            std::string prefix;
+            printAllWords(prefix);
+        }
+
+        void autocomplete(std::string prefix) {
+            TrieNode* tmp = this;
+            for(char c : prefix) {
+                if(tmp->children.find(c) == tmp->children.end())
+                    return;
+                tmp = tmp->children[c];
+            }
+            tmp->printAllWords(prefix);
+        }
+    private:
+        bool wordEnd;
+        std::unordered_map<char, TrieNode*> children;
+
+        void free() {
+            for(auto branch : this->children) {
+                branch.second->free();
+            }
+            delete this;
+        }
+
+        bool find(const std::string& word, int pos) {
+            if(word.size() == pos) {
+                return this->wordEnd;
+            }
+            
+            char key = word[pos];
+            if(this->children.find(key) == this->children.end()) {
+                return false;
+            }
+            return this->children[key]->find(word, pos+1);
+        }
+
+        void insert(const std::string& word, int pos) {
+            if(word.size() == pos) {
+                this->wordEnd = true;
+                return;
+            }
+
+            char key = word[pos];
+            if(this->children.find(key) == this->children.end()) {
+                this->children[key] = new TrieNode();
+            }
+            this->children[key]->insert(word, pos+1);
+        }
+
+        void printAllWords(std::string& prefix) {
+            if(this->wordEnd) {
+                std::cout << prefix << std::endl;
+            }
+
+            for(auto child : children) {
+                prefix.push_back(child.first);
+                child.second->printAllWords(prefix);
+                prefix.pop_back();
+            }
+        }
+};
+
+int main() {
+    TrieNode* trie = new TrieNode();
+
+    trie->insert("klima");
+    trie->insert("klizanje");
+    trie->insert("kriza");
+
+    trie->printAllWords();
+
+    std::string autocomplete;
+    std::cin >> autocomplete;
+
+    trie->autocomplete(autocomplete);
+    
+    delete trie;
+}
